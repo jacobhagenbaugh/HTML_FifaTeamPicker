3f97e1ad16a7991596dbda2bfa246f6485a30af0
Jacob Hagenbaugh <42615859+jacobhagenbaugh@users.noreply.github.com>
2018-11-30 01:52:16 -0500

    Added player variables as well as teamList, also added text fields in HTML.

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+var player1 = document.getElementById("player1-input");
+var player2 = document.getElementById("player2-input");
+var teamList = [];
