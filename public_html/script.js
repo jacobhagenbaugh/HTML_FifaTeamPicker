9ee7907cd36cc0e6d39354e4c5d1ca8c3dd9ee14
Jacob Hagenbaugh <42615859+jacobhagenbaugh@users.noreply.github.com>
2018-12-01 18:59:56 -0500

    Added a way for player names to be stored.

--- public_html/script.js
+++ public_html/script.js
@@ -1,17 +1,21 @@
 /* global gen */
-
-var player1 = document.getElementById("player1-input");
-var player2 = document.getElementById("player2-input");
 var teamList = ['Manchester City', 'Manchester United', 'Arsenal', 'Chelsea', 
                 'Liverpool', 'Tottenham', 'Barcelona', 'Real Madrid', 
                 'Athletico Madrid', 'Dortmund', 'Bayern Munich', 'PSG', 'Juventus'];
 
 // Methods for generating a new random team from the teamList array.
-//var randomIndex = Math.floor(Math.random() * teamList.length);
-//var randomTeam = teamList[randomIndex];
-
 function randomTeamGen() {
     var randomIndex = Math.floor(Math.random() * teamList.length);
     var randomTeam = teamList[randomIndex];
     return randomTeam;
+}
+
+var player1 = document.getElementById("player1-input").value;
+var player2 = document.getElementById("player2-input").value;
+
+function getPlayers() {
+    document.getElementById('player1').innerHTML = 
+        document.getElementById("player1-input").value;
+    document.getElementById('player2').innerHTML = 
+        document.getElementById("player2-input").value;
 }
