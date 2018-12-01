84f4703f306cbf82f966f564285b778496dca311
Jacob Hagenbaugh <42615859+jacobhagenbaugh@users.noreply.github.com>
2018-12-01 01:32:23 -0500

    Added the teamList array, created a function to generate a new team.
    Also put some tests in the HTML to make sure everything works okay.

--- public_html/script.js
+++ public_html/script.js
@@ -1,3 +1,17 @@
+/* global gen */
+
 var player1 = document.getElementById("player1-input");
 var player2 = document.getElementById("player2-input");
-var teamList = [];
+var teamList = ['Manchester City', 'Manchester United', 'Arsenal', 'Chelsea', 
+                'Liverpool', 'Tottenham', 'Barcelona', 'Real Madrid', 
+                'Athletico Madrid', 'Dortmund', 'Bayern Munich', 'PSG', 'Juventus'];
+
+// Methods for generating a new random team from the teamList array.
+//var randomIndex = Math.floor(Math.random() * teamList.length);
+//var randomTeam = teamList[randomIndex];
+
+function randomTeamGen() {
+    var randomIndex = Math.floor(Math.random() * teamList.length);
+    var randomTeam = teamList[randomIndex];
+    return randomTeam;
+}
